091cbac33ccf28103a75a4474baf6ab10ec736f7
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2024-01-03 03:01:53 +0000

    Parameter defaults

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,3 +1,3 @@
-export default function getSumOfHoods(initialNumber, expansion1908 = 89, expansion2019 = 19) {
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
   return initialNumber + expansion1908 + expansion2019;
 }
