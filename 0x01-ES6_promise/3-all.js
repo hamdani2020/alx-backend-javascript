e511ad4e4e413ee60c93ccfde5a385e68db37958
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2024-01-04 14:22:04 +0000

    Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -6,5 +6,5 @@ export default function handleProfileSignup() {
     .then((res) => {
       console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
     })
-    .catch(() => console.log('The user is offline'));
+    .catch(() => console.log('Signup system offline'));
 }
