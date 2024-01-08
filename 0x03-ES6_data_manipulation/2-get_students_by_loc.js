08518adf66a45d54ef72b5833176d721cd9df3a1
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2024-01-08 22:02:35 +0000

    task 2

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,11 @@
+/**
+ * This function returns an array of
+ * objects who are located in a specific city
+ */
+
+export default function getStudentsByLocation(students, city) {
+  if (students instanceof Array) {
+    return students.filter((student) => student.location === city);
+  }
+  return [];
+}
