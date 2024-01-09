6a6f34b7ab7311fa431a881259b8d55e44b79fec
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2024-01-09 06:13:28 +0000

    task 0

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,12 @@
+/**
+ * Displays the list of student
+ * @return {id, firstName, lacation}
+ */
+
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+}
