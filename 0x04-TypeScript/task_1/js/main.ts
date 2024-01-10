745a1fb2e97153223c0903c5cd38ef43c1e143ab
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2024-01-10 19:06:42 +0000

    task 1

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,47 @@
+export interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [index:string]: any;
+}
+
+export interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+export interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+export function printTeacher(firstName: string, lastName: string): string {
+  return `${firstName[0]}. ${lastName}`;
+}
+
+export interface isStudentClassConstructor {
+  new (firstName: string, lastName: string): isStudentClass;
+}
+
+export interface isStudentClass {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+export class StudentClass implements isStudentClass {
+  private _firstName! = string;
+  private _lastName! = string;
+
+  constructor(firstName: string, lastName: string) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+  }
+
+  workHomework() {
+    return 'Currently working';
+  }
+
+  displayName() {
+    this._firstName;
+  }
+}
