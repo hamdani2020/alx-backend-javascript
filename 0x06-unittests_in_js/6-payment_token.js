6c762752f2ca8baac86c964d1f69e1466fae08ae
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2024-02-29 19:38:04 +0000

    Async tests with done

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,6 +1,6 @@
 const getPaymentTokenFromAPI = (success) => new Promise((resolve, _reject) => {
   if (success) {
-    resolve({data: 'Successful response from API'});
+    resolve({data: 'Successful response from the API'});
   }
 });
 
